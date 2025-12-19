4bbeaf769f8da4b4630458943bea40303cc8b6d2
FranciscoEscano <francisco.escano@2itesting.com>
2025-12-19 10:33:25 +0000

    generating_test_reports

---
+++ module_3/pytest_framework/generating_test_reports/generating_test_reports.py
@@ -0,0 +1,14 @@
+# 1. Installing pytest-html
+#     pip install pytest-html
+
+# 2. Generating a Basic HTML Report
+#     pytest --html=report.html
+
+# 3. Adding Verbose Output
+#     pytest -v --html=report.html
+
+# 4. Customizing the HTML Report
+#     pytest --html=report.html --self-contained-html --title="Pytest Report"
+
+# 6. Combining with Coverage
+#     pytest --cov=my_module --html=report.html --self-contained-html
