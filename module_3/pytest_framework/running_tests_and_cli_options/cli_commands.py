93ff20ec60f27870d5e92485e1a21d28c26dea55
FranciscoEscano <francisco.escano@2itesting.com>
2025-12-19 10:11:24 +0000

    running_tests_and_cli_options

---
+++ module_3/pytest_framework/running_tests_and_cli_options/cli_commands.py
@@ -0,0 +1,17 @@
+# 1. Running a Single Test File
+#     pytest test_example.py
+
+# 2 Verbose Output (-v)
+#     pytest -v test_example.py
+
+# 3. Show Print Statements (-s)
+#     pytest -s test_example.py
+
+# 4. Stop on First Failure (-x)
+#     pytest -x test_example.py
+
+# 5. Running Specific Tests
+#     pytest test_example.py::test_addition
+
+# 7. Running All Tests in a Directory
+#     pytest tests/
